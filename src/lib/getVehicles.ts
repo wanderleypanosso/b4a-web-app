379077c16986850e7e831bd87ba9b69060ba5875
Wanderley Panosso <wanderley.panosso@gmail.com>
2024-08-15 17:54:50 +0000

    Update GET_VEHICLES query to use edges and node

--- src/lib/getVehicles.ts
+++ src/lib/getVehicles.ts
@@ -1,13 +1,15 @@
 import { gql } from '@apollo/client';
 
 export const GET_VEHICLES = gql`
-  query getVehicles($limit: Int, $skip: Int) {
-    vehicles(limit: $limit, skip: $skip) {
-      results {
-        objectId
-        make
-        model
-        year
+  query getVehicles {
+    vehicles {
+      edges {
+        node {
+          objectId
+          make
+          model
+          year
+        }
       }
     }
   }
