877653f2e6a6f581dc1478d8cb036da0814f8158
wanderleypanosso <wanderley.panosso@gmail.com>
2024-08-15 14:12:42 +0000

    Fix Apollo Client to use GraphQL env variables

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,11 +1,12 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://parseapi.back4app.com/graphql',
+  uri: "https://parseapi.back4app.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    'X-Parse-Application-Id': 'YHSRARrlmxOYlYKzi0vWKVjWRINqwP00ofqX8hFP',
-    'X-Parse-REST-API-Key': 'jWoQ1V5klwd8kTg3tqU5odb7FrRtpq4WmWYQwKRc',
+    "X-Parse-Application-Id": process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID,
+    "X-Parse-Master-Key": process.env.NEXT_PUBLIC_PARSE_MASTER_KEY,
+    "X-Parse-Client-Key": process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY,
   },
 });
 
