c80a5c20f415083a7f14ae2213eb74bcac870273
Wanderley Panosso <wanderley.panosso@gmail.com>
2024-08-15 17:07:31 +0000

    Fix Apollo Client ensuring env variables are used correcting valid paths

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -11,9 +11,9 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      'X-Parse-Application-Id': process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID,
-      'X-Parse-Master-Key': process.env.NEXT_PUBLIC_PARSE_MASTER_KEY,
-      'X-Parse-Client-Key': process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY,
+      'X-Parse-Application-Id': process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID || '',
+      'X-Parse-Master-Key': process.env.NEXT_PUBLIC_PARSE_MASTER_KEY || '',
+      'X-Parse-Client-Key': process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY || '',
     }
   }
 });
