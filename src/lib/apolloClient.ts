eea00da39e4e02d8b23a736a1a6ae4191d9b9d25
Wanderley Panosso <wanderley.panosso@gmail.com>
2024-08-15 17:09:57 +0000

    Revert Apollo Client to previous version

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,26 +1,13 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import fetch from 'cross-fetch';
-
-const httpLink = createHttpLink({
-  uri: 'https://parseapi.back4app.com/graphql',
-  fetch,
-});
-
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      'X-Parse-Application-Id': process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID || '',
-      'X-Parse-Master-Key': process.env.NEXT_PUBLIC_PARSE_MASTER_KEY || '',
-      'X-Parse-Client-Key': process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY || '',
-    }
-  }
-});
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  uri: 'https://parseapi.back4app.com/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    'X-Parse-Application-Id': process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID || '',
+    'X-Parse-Master-Key': process.env.NEXT_PUBLIC_PARSE_MASTER_KEY || '',
+    'X-Parse-Client-Key': process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY || '',
+  },
 });
 
 export default client;
