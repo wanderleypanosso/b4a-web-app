3f45a94c9bdda60f58acd5cde5c050ad08fc10dd
wanderleypanosso <wanderley.panosso@gmail.com>
2024-08-15 18:15:51 +0000

    Fix initializeApollo export and refactor Home page

---
+++ src/lib/initializeApollo.ts
@@ -0,0 +1,15 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const initializeApollo = () => {
+  return new ApolloClient({
+    uri: 'https://parseapi.back4app.com/graphql',
+    cache: new InMemoryCache(),
+    headers: {
+      'X-Parse-Application-Id': process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID || '',
+      'X-Parse-Master-Key': process.env.NEXT_PUBLIC_PARSE_MASTER_KEY || '',
+      'X-Parse-Client-Key': process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY || '',
+    },
+  });
+};
+
+export default initializeApollo;
