491687502eb2ed24b285c380be6af7cb674c857d
Your Name <you@example.com>
2024-08-15 14:24:26 +0000

    Fix Apollo Client to ensure env variables are used for server build

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,13 +1,26 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import fetch from 'cross-fetch';
+
+const httpLink = createHttpLink({
+  uri: 'https://parseapi.back4app.com/graphql',
+  fetch,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'X-Parse-Application-Id': process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID,
+      'X-Parse-Master-Key': process.env.NEXT_PUBLIC_PARSE_MASTER_KEY,
+      'X-Parse-Client-Key': process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY,
+    }
+  }
+});
 
 const client = new ApolloClient({
-  uri: "https://parseapi.back4app.com/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-  headers: {
-    "X-Parse-Application-Id": process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID,
-    "X-Parse-Master-Key": process.env.NEXT_PUBLIC_PARSE_MASTER_KEY,
-    "X-Parse-Client-Key": process.env.NEXT_PUBLIC_PARSE_CLIENT_KEY,
-  },
 });
 
 export default client;
