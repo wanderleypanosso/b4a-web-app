8fa26ba833a28f0b0d9e8dca231008ca748a02aa
Your Name <wanderley.panosso@gmail.com>
2024-08-16 19:39:16 +0000

    Update GraphQL mutation, fix lint errors

--- src/lib/api/vehicles.ts
+++ src/lib/api/vehicles.ts
@@ -2,10 +2,8 @@ import { gql } from '@apollo/client';
 import client from '../apolloClient';
 
 const CREATE_VEHICLE_MUTATION = gql`
-  mutation CreateVehicle($name: String!, $color: String!, $price: Float!, $year: String!) {
-    createVehicle(input: {
-      fields: { name: $name, color: $color, price: $price, year: $year }
-    }) {
+  mutation CreateObject($fields: VehicleCreateInput!) {
+    createVehicle(input: { fields: $fields }) {
       vehicle {
         id
         name
@@ -20,7 +18,7 @@ const CREATE_VEHICLE_MUTATION = gql`
 export async function createVehicle(name: string, color: string, price: number, year: string) {
   const { data } = await client.mutate({
     mutation: CREATE_VEHICLE_MUTATION,
-    variables: { name, color, price, year }
+    variables: { fields: { name, color, price, year } }
   });
   return data.createVehicle.vehicle;
 }
