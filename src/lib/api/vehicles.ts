5492067c95b7d683289beaae3b0197076547729d
Wanderley Panosso <wanderley.panosso@gmail.com>
2024-08-16 18:49:12 +0000

    Update and lint code for using GraphQL API to create vehicles

--- src/lib/api/vehicles.ts
+++ src/lib/api/vehicles.ts
@@ -3,12 +3,16 @@ import client from '../apolloClient';
 
 const CREATE_VEHICLE_MUTATION = gql`
   mutation CreateVehicle($name: String!, $color: String!, $price: Float!, $year: String!) {
-    createVehicle(fields: { name: $name, color: $color, price: $price, year: $year }) {
-      objectId
-      name
-      color
-      price
-      year
+    createVehicle(input: {
+      fields: { name: $name, color: $color, price: $price, year: $year }
+    }) {
+      vehicle {
+        objectId
+        name
+        color
+        price
+        year
+      }
     }
   }
 `;
@@ -18,5 +22,5 @@ export async function createVehicle(name: string, color: string, price: number, 
     mutation: CREATE_VEHICLE_MUTATION,
     variables: { name, color, price, year }
   });
-  return data.createVehicle;
+  return data.createVehicle.vehicle;
 }
